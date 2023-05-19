6a8a715bc99d35df3a6a80cdc2ea985322ef7f97
jihwankim128 <jihwan1535@nate.com>
2023-05-19 23:32:01 +0900

    LowerBound or TwoPointer

--- boj/gold/이분탐색/2470 (두 용액).cpp
+++ boj/gold/이분탐색/2470 (두 용액).cpp
@@ -5,6 +5,7 @@ using namespace std;
 int n;
 int arr[100001];
 
+// lower Bound
 void input() {
 	cin >> n;
 	for(int i = 0; i<n; i++) {
@@ -49,6 +50,7 @@ void ps() {
 	int y = arr[p.second];
 	cout << min(x,y) << " " << max(x, y);
 }
+
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
@@ -58,3 +60,45 @@ int main() {
 	
 	return 0;
 }
+
+/* Two Pointer
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int n;
+int arr[100001];
+
+void input() {
+	cin >> n;
+	for(int i = 0; i<n; i++) cin >> arr[i];
+	sort(arr, arr+n);
+}
+
+int zero = INT_MAX;
+void solve() {
+	int l= 0; int r = n-1;
+	int ph8, ph6;
+	for(l; l < r; ) {
+		if(zero > abs(arr[l]+arr[r])) {
+			zero = abs(arr[l]+arr[r]);
+			ph8 = arr[l];
+			ph6 = arr[r];
+		}
+		if(arr[l] + arr[r] > 0) r--;
+		else l++;
+	}
+	cout << ph8 << " " << ph6;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	input();
+	solve();
+	
+	return 0;
+}
+*/
