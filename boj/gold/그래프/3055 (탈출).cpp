f5fc7e79f54e567e0fc66e0038c376ab92056cdb
jihwankim128 <jihwan1535@nate.com>
2023-05-19 23:17:32 +0900

    Multi-Source BFS

--- boj/gold/그래프/3055 (탈출).cpp
+++ boj/gold/그래프/3055 (탈출).cpp
@@ -2,30 +2,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-int r, c;
-int arr[51][51];
-bool visited[51][51];
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, -1, 1};
-
-pair<int, int> start;
-
-void input() {
-	cin >> r >> c;
-	for(int i = 0; i<r; i++) {
-		string s;
-		cin >> s;
-		for(int j = 0; j<c; j++) {
-			if(s[j] == 'D') arr[i][j] = 2;
-			else if(s[j] == '.') arr[i][j] = 0;
-			else if(s[j] == '*') arr[i][j] = -1;
-			else if(s[j] == 'X') arr[i][j] = -2;
-			else {
-				start = {i, j};
-				arr[i][j] = 1;
-			}
-=======
 int n, m;
 int graph[51][51];
 bool visited[51][51];
@@ -45,46 +21,10 @@ void input() {
 			if(s[j] == 'D') terminal = {i, j};
 			if(s[j] == 'S') hog = {i, j};
 			if(s[j] == '*') water.push({i, j});
->>>>>>> c19c37cc2b4d34341e7cd72e97cc8c7ad787fcf3
 		}
 	}
 }
 
-<<<<<<< HEAD
-void water() {
-	for(int i = 0; i<r; i++) {
-		for(int j = 0; j<c; j++) {
-			if(arr[i][j] == -1) {
-				for(int k = 0; k<4; k++) {
-					int ni = i+dx[k]; int nj = j+dy[k]; 
-					if(ni < 0 || ni >= r) continue;
-					if(nj < 0 || nj >= c) continue;
-					if(!visited[ni][nj] && arr[ni][nj] == 0) {
-						visited[ni][nj] = true;
-					}
-				}
-			}
-		}
-	}
-}
-
-void bfs() {
-	queue<pair<int, int> > q;
-	q.push(start);
-	
-	while(!q.empty()) {
-		water();
-		int x = q.front().first;
-		int y = q.front().second; q.pop();
-		for(int i = 0; i<4; i++) {
-			int nx = x+dx[i]; int ny = y+dy[i];
-			if(nx < 0 || nx >= r) continue;
-			if(ny < 0 || ny >= c) continue;
-			
-		}
-		
-	} 
-=======
 void bfs_water() {
 	graph[terminal.first][terminal.second] = -1;
 	while(!water.empty()) {
@@ -144,19 +84,14 @@ void solve() {
 	//print();
 	if(graph[terminal.first][terminal.second] == 255) cout << "KAKTUS";
 	else cout << graph[terminal.first][terminal.second];
->>>>>>> c19c37cc2b4d34341e7cd72e97cc8c7ad787fcf3
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
-<<<<<<< HEAD
-	cin.tie(NULL);	
-=======
 	cin.tie(NULL);
 	
 	input();
 	solve();
 	
 	return 0;
->>>>>>> c19c37cc2b4d34341e7cd72e97cc8c7ad787fcf3
 }
