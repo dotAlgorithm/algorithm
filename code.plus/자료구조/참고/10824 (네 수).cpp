cb4255a0d2d642f5b66a5bea65719c8c83c214a1
jihwankim128 <jihwan1535@nate.com>
2023-05-19 22:27:16 +0900

    String

--- code.plus/자료구조/참고/10824 (네 수).cpp
+++ code.plus/자료구조/참고/10824 (네 수).cpp
@@ -17,7 +17,40 @@ int main() {
 	for(int i=0;i<2;i++){
 		cin >> str;
 		b+=str;
+	}a#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	string a;
+	string b;
+	string str;
+	
+	for(int i=0;i<2;i++){
+		cin >> str;
+		a+=str;
 	}
+	for(int i=0;i<2;i++){
+		cin >> str;
+		b+=str;
+	}
+	long long int num1=0;
+	long long int num2=0;
+	for(int i = 0; i<a.size(); i++) {
+		int size = a.size()-1;
+		num1 += 0<size-i ? (a[i]-'0')*pow(10,size-i) : a[i]-'0';
+	}
+	for(int i = 0; i<b.size(); i++) {
+		int size = b.size()-1;
+		num2 += 0<size-i ? (b[i]-'0')*pow(10,size-i) : b[i]-'0';
+	}
+	cout << num1+num2;
+	
+	return 0;
+}
 	long long int num1=stoll(a);
 	long long int num2=stoll(b);
 	
