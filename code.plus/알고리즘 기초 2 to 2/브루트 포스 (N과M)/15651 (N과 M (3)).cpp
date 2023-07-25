3bca7547493d50b96adea6fee9493f73e6495173
jihwankim128 <jihwan1535@nate.com>
2023-07-26 06:56:01 +0900

    N^M Time Complex

--- code.plus/알고리즘 기초 2 to 2/브루트 포스 (N과M)/15651 (N과 M (3)).cpp
+++ code.plus/알고리즘 기초 2 to 2/브루트 포스 (N과M)/15651 (N과 M (3)).cpp
@@ -1,26 +1,37 @@
+// acmicpc 15651
+// authored by jihwankim
+// code.plus
+// 23-07-26 AM 05:34
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int n, m;
-int arr[8];
+int arr[7];
 
-void out(int len){
-	if(len > m) {
-		for(int i = 1; i<=m; i++) {
+void solve(int k) {
+	if(k == m) {
+		for(int i = 0; i<k; i++) {
 			cout << arr[i] << " ";
-		} cout <<"\n";
-	}
-	else {
-		for(int i = 1; i<=n; i++){
-			arr[len] = i;
-			out(len+1);
 		}
+		cout << "\n";
+		return ;
+	}
+	
+	for(int i = 1; i<=n; i++) {
+		arr[k] = i;
+		solve(k+1);
 	}
 }
 
-int main(){
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
 	cin >> n >> m;
-	out(1);
+	solve(0);
+	
 	return 0;
 }
+// AM 05:37
