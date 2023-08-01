ade31c440622e44b22fd72e5390d66fb8de0c5c2
jihwankim128 <jihwan1535@nate.com>
2023-08-02 01:48:56 +0900

    N과 M

---
+++ code.plus/알고리즘 기초 2 to 2/브루트 포스 (N과M)/15654 (N과 M (5)).cpp
@@ -0,0 +1,50 @@
+// acmicpc 15654
+// authored by jihwankim
+// 23-07-27 AM 08:13
+// code.plus
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[10001];
+int save[10001];
+bool chk[9];
+int n, m;
+
+void input() {
+	cin >> n >> m;
+	for(int i = 0; i<n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr+n);
+}
+
+void solve(int k) {
+	if(k == m) {
+		for(int i = 0; i<k; i++) {
+			cout << save[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for(int i = 0; i<n; i++) {
+		if(!chk[i]) {
+			save[k] = arr[i];
+			chk[i] = true;
+			solve(k+1);
+			chk[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	input();
+	solve(0);
+	
+	return 0;
+}
+
+// AM 08:19
