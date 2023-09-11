f2bfbaeb18242f22cab0ed33474e56e31192122f
jihwankim128 <jihwan1535@nate.com>
2023-09-12 06:43:31 +0900

    dfs, bruteforce

--- code.plus/알고리즘 기초 2 to 2/그래프 1/13023 (ABCDE).cpp
+++ code.plus/알고리즘 기초 2 to 2/그래프 1/13023 (ABCDE).cpp
@@ -1,46 +1,53 @@
+// acmicpc 13023
+// authored by jihwankim
+// code.plus
+// 23-09-07 PM 05:00
 #include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> graph[2001];
-int visited[2001];
 int n, m;
+vector<int> arr[2005];
+bool visited[2005];
 
 void input() {
 	cin >> n >> m;
-	for(int i = 0; i<m; i++){
+	for(int i = 0; i<m; i++) {
 		int a, b;
 		cin >> a >> b;
-		graph[a].push_back(b);
-		graph[b].push_back(a);
+		arr[a].push_back(b);
+		arr[b].push_back(a);
 	}
 }
 
-int solve(int k, int cnt){
-	if(cnt == 5) return 1;
-	for(int i = 0; i<graph[k].size(); i++) {
-		if(!visited[graph[k][i]]) {
-			visited[graph[k][i]] = 1;
-			if(solve(graph[k][i], cnt+1)==1) return 1;
-			visited[graph[k][i]] = 0;
+bool rec(int v, int k) {
+	if(k == 5) return true;
+	for(int i = 0; i<arr[v].size(); i++) {
+		if(!visited[arr[v][i]]) {
+			visited[arr[v][i]] = true;
+			if(rec(arr[v][i], k+1)) return true;
+			visited[arr[v][i]] = false;
 		}
-	}	
-	return 0;
+	}
+	return false;
+}
+
+bool solve() {
+	for(int i = 0; i<n; i++) {
+		visited[i] = true;
+		if(rec(i, 1)) return true;
+		visited[i] = false;
+	}
+	return false;
 }
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
 	
 	input();
+	cout << solve();
 	
-	for(int i = 0; i<n; i++) {
-		visited[i] = 1;
-		if(solve(i, 1) == 1){
-			cout << "1";
-			return 0;
-		}	
-		visited[i] = 0;
-	}
-	cout << "0";
 	return 0;
 }
+
+// 05:37
