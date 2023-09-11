c691589a4f13edeaf8d17e5783cba8b20a5a4a85
jihwankim128 <jihwan1535@nate.com>
2023-09-12 06:42:54 +0900

    dfs, bruteforce

--- code.plus/알고리즘 기초 2 to 2/그래프 1/11724 (연결 요소의 개수).cpp
+++ code.plus/알고리즘 기초 2 to 2/그래프 1/11724 (연결 요소의 개수).cpp
@@ -1,73 +1,54 @@
+// acmicpc 11724
+// authored by jihwankim
+// code.plus
+// 23-09-08 AM 03:57
 #include <bits/stdc++.h>
 
 using namespace std;
 
-<<<<<<< HEAD
-int graph[1001][1001];
 int n, m;
-=======
-int n, m;
-bool visited[1001];
-vector<int> graph[1001];
-queue<int> q;
-
-void bfs(int n){
-	q.push(n);
-	visited[n] = true;
-	while(!q.empty()){
-		int v = q.front(); q.pop();
-		for(int i = 0; i<graph[v].size(); i++){
-			int u = graph[v][i];
-			if(!visited[u]){ 
-				q.push(u);
-				visited[u] = true;	
-			}
-		}
-	}
-}
+vector<int> arr[1005];
+bool visited[1005];
 
 void input() {
 	cin >> n >> m;
-	for(int i = 0; i<m; i++){
-		int u, v;
-		cin >> u >> v;
-		graph[u].push_back(v);
-		graph[v].push_back(u);
+	for(int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		arr[a].push_back(b);
+		arr[b].push_back(a);
+	}
+}
+
+void dfs(int x) {
+	for(auto &i : arr[x]) {
+		if(!visited[i]) {
+			visited[i] = true;
+			dfs(i);
+		}
 	}
 }
 
 void solve() {
 	int cnt = 0;
-	for(int i = 1; i<=n; i++) {
+	for(int i = 1; i <= n; i++) {
 		if(!visited[i]) {
-			bfs(i);
 			cnt++;
+			visited[i] = true;
+			dfs(i);
 		}
 	}
-	cout << cnt;	
+	cout << cnt;
 }
->>>>>>> c19c37cc2b4d34341e7cd72e97cc8c7ad787fcf3
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
 	
-<<<<<<< HEAD
-	int cnt = 0;
-	
-	cin >> n >> m;
-	
-	for(int i = 0; i<m; i++) {
-		int u, v;
-		cin >> u >> v;
-		if(graph[v][u]) cnt++;
-		graph[u][v] = 1;
-	}
-	cout << cnt;
-
-=======
 	input();
-	solve();
->>>>>>> c19c37cc2b4d34341e7cd72e97cc8c7ad787fcf3
+	solve()	;	
+	
 	return 0;
 }
+
+// AM 04:02 
