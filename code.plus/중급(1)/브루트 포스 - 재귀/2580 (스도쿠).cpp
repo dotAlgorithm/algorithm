fc59b5a1589d6f180300e9b6a59e8edb2a36770f
jihwankim128 <jihwan1535@nate.com>
2023-09-28 03:08:59 +0900

    TimeOver, recursion

--- code.plus/중급(1)/브루트 포스 - 재귀/2580 (스도쿠).cpp
+++ code.plus/중급(1)/브루트 포스 - 재귀/2580 (스도쿠).cpp
@@ -14,7 +14,88 @@ int cnt;
 
 void input() {
 	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < 9; j++) {¤±// acmicpc 2580
+// authored by jihwankim
+// code.plus
+// 23-09-22 AM 04:01
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[9][9];
+int cnt;
+vector<pair<int, int>> point; 
+void input() {
+	for(int i = 0; i < 9; i++) {
 		for(int j = 0; j < 9; j++) {
+			cin >> arr[i][j];
+			if(arr[i][j] == 0) point.push_back({i, j});
+		}
+	}
+}
+
+bool find_row(int i, int j) {
+	for(int k = 0; k < 9; k++) {
+		if(k == i) continue;
+		if(arr[k][j] == arr[i][j]) return false;
+	}
+	return true;
+} 
+
+bool find_col(int i, int j) {
+	for(int k = 0; k < 9; k++) {
+		if(k == j) continue;
+		if(arr[i][k] == arr[i][j]) return false;
+	}
+	return true;
+}
+
+bool find_33(int xfrom, int yfrom, int x, int y) {
+	for(int i = xfrom; i < xfrom+3; i++) {
+		for(int j = yfrom; j < yfrom+3; j++) {
+			if(x == i && y == j) continue;
+			if(arr[i][j] == arr[x][y]) return false;
+		}
+	}
+	return true;
+}
+
+bool solve(int k) {
+	if(k == point.size()) return true;
+	
+	int x = point[k].first;
+	int y = point[k].second;
+	for(int i = 1; i < 10; i++) {
+		arr[x][y] = i;
+		if(!find_row(x, y)) continue;
+		if(!find_col(x, y)) continue;
+		int xfrom = 3 * (x/3); int yfrom = 3 * (y/3);
+		if(!find_33(xfrom, yfrom, x, y)) continue;
+		
+		if(solve(k+1)) return true;
+	}
+	arr[point[k].first][point[k].second] = 0;
+
+	return false;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	
+	input();
+	if(solve(0)) {
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 9; j++) {
+				cout << arr[i][j] << " ";
+			}
+			cout << '\n';
+		}
+	}
+	
+	return 0;
+}
+// fail 
 			cin >> arr[i][j];
 			if(arr[i][j] == 0) continue;
 			col[i][arr[i][j]] = true;
