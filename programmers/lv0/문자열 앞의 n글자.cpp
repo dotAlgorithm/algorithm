3148bf1322781aa9f498dd6d26e8a79cb558b1db
jihwankim128 <jihwan1535@nate.com>
2023-10-31 09:42:35 +0900

    string.substr -> index부터 n글자의 부분문자열

---
+++ programmers/lv0/문자열 앞의 n글자.cpp
@@ -0,0 +1,15 @@
+// authored by jihwankim
+// 23-10-30 AM 07:49
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    return my_string.substr(0, n);
+}
+/*
+my_string과 정수 n이 주어짐
+my_string 앞의 n글자로 이루어진 문자열을 리턴
+*/
+// AM 07:50
