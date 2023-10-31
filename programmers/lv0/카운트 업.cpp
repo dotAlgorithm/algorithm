52a89e9d73f574316c2166e017293ff9270ad87c
jihwankim128 <jihwan1535@nate.com>
2023-10-31 11:03:31 +0900

    iota() 알아보기

---
+++ programmers/lv0/카운트 업.cpp
@@ -0,0 +1,16 @@
+// authored by jihwankim
+// 23-10-31 AM 11:01
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int i, int n) {
+    vector<int> answer;
+    for(i; i<=n; i++) answer.push_back(i);
+    return answer;
+}
+/*
+start to end
+*/
+// AM 11:02
