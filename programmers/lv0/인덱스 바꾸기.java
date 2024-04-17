49f0dab70db827a3b754896ebe7694b8efc3599f
jihwan <jihwan1535@nate.com>
2024-04-17 16:29:21 +0900

    swap index in String
    subString을 활용
    
    toCharArray()로 변경 후 index swap한다. String.valueOf로 String으로 변환한다.
    
    String을 한 글자 String List로 변환한다.
    Arrays.stream(String.split()).collect(Collectors.toList());
    Collections.swap(String, index, index);
    String.join(, List<String>);
    
    StringBuilder를 활용한 방법도 있다.
    new StringBuilder(String);
    StringBuilder.setCharAt(index, Character);
    StringBuilder.toString();

---
+++ programmers/lv0/인덱스 바꾸기.java
@@ -0,0 +1,9 @@
+// 24-04-17 PM 04:22 ~ 04:25
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        return my_string.substring(0, num1) + my_string.charAt(num2) 
+            + my_string.substring(num1+1, num2) + my_string.charAt(num1) 
+            + my_string.substring(num2+1);
+    }
+}
